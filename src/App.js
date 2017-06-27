6f065a12b48542a57baaf5d16ce9c8a1c8e7763f
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-06-27 23:06:43 +1200

    Added Admin on rest

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
+import { Admin } from 'admin-on-rest';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Smart Grocery</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Admin />
     );
   }
 }
