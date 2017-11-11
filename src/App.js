95d8fccf5dc78cec4905d19b1d5ca1237bc9ac23
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-11-11 22:51:58 +1300

    Added error message handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // @flow
 
+import Immutable, { Map } from 'immutable';
 import * as userAccessActions from 'micro-business-parse-server-common-web/dist/userAccess/Actions';
 import { UserAccessStatus } from 'micro-business-parse-server-common-web';
+import { MessageType } from 'micro-business-common-react';
+import * as messageBarActions from 'micro-business-common-react/dist/messageBar/Actions';
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Snackbar from 'material-ui/Snackbar';
 import { LinearProgress } from 'material-ui/Progress';
+import Fade from 'material-ui/transitions/Fade';
 import { withStyles } from 'material-ui/styles';
 import withRoot from './sharedComponents/withRoot';
 import { HomeContainer } from './app/home';
@@ -16,12 +21,49 @@ import Styles from './Styles';
 import './App.css';
 
 class App extends Component {
+  state = {
+    snackbar: {
+      open: false,
+      errorMessageToDisplay: null,
+    },
+  };
+
   componentWillMount = () => {
     this.props.userAccessActions.getCurrentUser();
   };
 
-  render = () =>
-    this.props.getCurrentUserStatus === UserAccessStatus.IN_PROGRESS ? (
+  componentWillReceiveProps = nextProps => {
+    const errorMessages = Immutable.fromJS(nextProps.messagesInfo)
+      .filter(messageInfo => messageInfo.get('messageType') === MessageType.ERROR)
+      .map(messageInfo => messageInfo.get('message'));
+
+    if (!errorMessages.isEmpty()) {
+      this.showSnackbar(errorMessages.reduce((acc, message) => acc + '\n' + message));
+    }
+
+    nextProps.messagesInfo.forEach(messageInfo => this.props.messageBarActions.remove(messageInfo.messageId));
+
+    nextProps.userAccessFailedOperations.forEach(failedOperation => {
+      this.props.messageBarActions.add(failedOperation.errorMessage, MessageType.ERROR);
+
+      this.props.userAccessActions.acknowledgeFailedOperation(
+        Map({
+          operationId: failedOperation.operationId,
+        }),
+      );
+    });
+  };
+
+  showSnackbar = errorMessageToDisplay => {
+    this.setState({ snackbar: { open: true, errorMessageToDisplay } });
+  };
+
+  handleRequestCloseSnackbar = () => {
+    this.setState({ snackbar: { open: false, errorMessageToDisplay: null } });
+  };
+
+  render = () => {
+    return this.props.getCurrentUserStatus === UserAccessStatus.IN_PROGRESS ? (
       <div className={this.props.classes.progressRoot}>
         <LinearProgress />
       </div>
@@ -31,24 +73,39 @@ class App extends Component {
           <Route path="/" component={HomeContainer} exact />
           <Route path="/signin" component={SignInContainer} exact />
           <Route path="/signup" component={SignUpContainer} exact />
+          <Snackbar
+            open={this.state.snackbar.open}
+            onRequestClose={this.handleRequestCloseSnackbar}
+            transition={Fade}
+            SnackbarContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            message={<span id="message-id">{this.state.snackbar.errorMessageToDisplay}</span>}
+          />
         </div>
       </BrowserRouter>
     );
+  };
 }
 
 App.propTypes = {
   getCurrentUserStatus: PropTypes.number.isRequired,
+  messageBarActions: PropTypes.object.isRequired,
+  userAccessActions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     getCurrentUserStatus: state.userAccess.get('getCurrentUserStatus'),
+    messagesInfo: state.messageBar.get('messages').toJS(),
+    userAccessFailedOperations: state.userAccess.get('failedOperations').toJS(),
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     userAccessActions: bindActionCreators(userAccessActions, dispatch),
+    messageBarActions: bindActionCreators(messageBarActions, dispatch),
   };
 }
 
