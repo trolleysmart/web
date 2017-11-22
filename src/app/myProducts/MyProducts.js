4d790877586f6d9ba3f3165db76ac3b26d7bd0fc
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-11-22 22:52:34 +1300

    Code Refactoring

--- src/app/myProducts/MyProducts.js
+++ src/app/myProducts/MyProducts.js
@@ -1,42 +1,38 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import { environment } from '../../framework/relay';
 import { graphql, QueryRenderer } from 'react-relay';
 import MyProductsRelayContainer from './MyProductsRelayContainer';
 import Loading from '../../sharedComponents/loading';
 
-class MyProducts extends Component {
-  render() {
-    return (
-      <QueryRenderer
-        environment={environment}
-        query={graphql`
-          query MyProductsQuery($count: Int!, $cursor: String) {
-            user {
-              ...MyProductsRelayContainer_user
-            }
-          }
-        `}
-        variables={{
-          cursor: null,
-          count: 30,
-        }}
-        render={({ error, props, retry }) => {
-          if (error) {
-            console.log(error);
-            return <div />;
-          }
+const MyProducts = () => (
+  <QueryRenderer
+    environment={environment}
+    query={graphql`
+      query MyProductsQuery($count: Int!, $cursor: String) {
+        user {
+          ...MyProductsRelayContainer_user
+        }
+      }
+    `}
+    variables={{
+      cursor: null,
+      count: 30,
+    }}
+    render={({ error, props, retry }) => {
+      if (error) {
+        console.log(error);
+        return <div />;
+      }
 
-          if (props) {
-            return <MyProductsRelayContainer user={props.user} />;
-          }
+      if (props) {
+        return <MyProductsRelayContainer user={props.user} />;
+      }
 
-          return <Loading />;
-        }}
-      />
-    );
-  }
-}
+      return <Loading />;
+    }}
+  />
+);
 
 export default MyProducts;
--- src/app/myStores/MyStores.js
+++ src/app/myStores/MyStores.js
@@ -1,42 +1,38 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import { environment } from '../../framework/relay';
 import { graphql, QueryRenderer } from 'react-relay';
 import MyStoresRelayContainer from './MyStoresRelayContainer';
 import Loading from '../../sharedComponents/loading';
 
-class MyStores extends Component {
-  render = () => {
-    return (
-      <QueryRenderer
-        environment={environment}
-        query={graphql`
-          query MyStoresQuery($count: Int!, $cursor: String) {
-            user {
-              ...MyStoresRelayContainer_user
-            }
-          }
-        `}
-        variables={{
-          cursor: null,
-          count: 30,
-        }}
-        render={({ error, props, retry }) => {
-          if (error) {
-            console.log(error);
-            return <div />;
-          }
+const MyStores = () => (
+  <QueryRenderer
+    environment={environment}
+    query={graphql`
+      query MyStoresQuery($count: Int!, $cursor: String) {
+        user {
+          ...MyStoresRelayContainer_user
+        }
+      }
+    `}
+    variables={{
+      cursor: null,
+      count: 30,
+    }}
+    render={({ error, props, retry }) => {
+      if (error) {
+        console.log(error);
+        return <div />;
+      }
 
-          if (props) {
-            return <MyStoresRelayContainer user={props.user} />;
-          }
+      if (props) {
+        return <MyStoresRelayContainer user={props.user} />;
+      }
 
-          return <Loading />;
-        }}
-      />
-    );
-  };
-}
+      return <Loading />;
+    }}
+  />
+);
 
 export default MyStores;
